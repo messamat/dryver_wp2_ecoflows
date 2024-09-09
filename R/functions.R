ccb40e45e6c2315f21147c75bc2eec77c335cc50
Mathis MEssager <mathis.messager@mail.mcgill.ca>
2024-09-09 18:25:32 +0200

    Format data for alpha diversity analysis

--- R/functions.R
+++ R/functions.R
@@ -1,18 +1,39 @@
 #-------------- workflow functions ---------------------------------------------
-read_biodt <- function(path_list) {
+#path_list = tar_read(bio_data_paths)
+
+#------ read_biodt -------------------------------------------------------------
+read_biodt <- function(path_list, in_metadata_edna) {
   #Read and name all data tables
   dt_list <- mapply(function(in_path, in_name) {
     fread(in_path) %>% 
       .[, organism := in_name]},
     path_list, names(path_list))
   
-  #Add Campaign and Site to bacteria data
-  dt_list$bac_sedi[, c('Site', 'Campaign') := tstrsplit(V1, '_')]
-  dt_list$bac_biof[, c('Site', 'Campaign') := tstrsplit(V1, '_')]
+  #Add Campaign and Site to bacteria data, then remove pool sites
+  dt_list$bac_sedi[, c('Site', 'Campaign') := tstrsplit(V1, '_')] %>%
+    setnames('V1', 'running_id')
+  dt_list$bac_biof[, c('Site', 'Campaign') := tstrsplit(V1, '_')] %>%
+    setnames('V1', 'running_id')
+  
+  #Remove bacteria in pools
+  dt_list$bac_sedi_nopools <- merge(dt_list$bac_sedi, 
+                                    in_metadata_edna[Sample_type=='sediment', 
+                                                     .(matchingEnvID, Habitat)],
+                                    by.x='running_id', by.y='matchingEnvID') %>%
+    .[Habitat!='pool',] %>%
+    .[, Habitat := NULL]
+  
+  dt_list$bac_biof_nopools <- merge(dt_list$bac_biof,
+                                    in_metadata_edna[Sample_type=='biofilm',
+                                                     .(matchingEnvID, Habitat)],
+                                    by.x='running_id', by.y='matchingEnvID') %>%
+    .[Habitat!='pool',] %>%
+    .[, Habitat := NULL]
   
   return(dt_list)
 }
 
+#------ calc_sprich ------------------------------------------------------------
 calc_sprich <- function(in_biodt, in_metacols) {
   metacols_sub <- names(in_biodt)[names(in_biodt) %in% in_metacols]
   biodt_melt <- melt(in_biodt, id.vars = metacols_sub)
@@ -22,22 +43,35 @@ calc_sprich <- function(in_biodt, in_metacols) {
   return(biodt_sprich)
 }
 
+#------ format_envinterm -------------------------------------------------------
+# in_env_dt <- tar_read(env_dt)
+# in_interm90_dt <- tar_read(interm90_dt)
+# in_sprich = tar_read(sprich)
 
-# 
-# 
-# com <- dia_sed[,c(3,8:1301)]
-# meta <- dia_sed[,1:7]
-# # first column as row names
-# com <- data.frame(com[,-1], row.names=com$running_id)
-# 
-# # diversity indices for running_id
-# dia_sed_S <- data.frame(specnumber(com))
-# dia_sed_S <- cbind(dia_sed_S, meta)
-# # mean diversity for each site
-# dia_sed_S_mean<- dia_sed_S %>%
-#   group_by(Site) %>%
-#   summarise(mean_S=mean(specnumber.com., na.rm = T))
-# dia_sed_S_mean$Organism <- "Sediment_diatoms"
-# 
-# rm(dia_sed, dia_sed_S)
-# rm(com2)
+merge_alphadat <- function(in_env_dt, in_interm90_dt) {
+
+  interm90_mean <- in_interm90_dt[
+    , list(totdur90 = mean(TotDur, na.rm=T),
+           TotLeng90 = mean(TotLeng, na.rm=T)),
+    by=Sites] %>%
+    setnames('Sites', 'site')
+
+  env_mean <- in_env_dt[
+    , list(discharge = mean(discharge_l_s, na.rm=T),
+           moss = mean(moss_cover, na.rm=T),
+           particlesize = mean(particle_size, na.rm=T)),
+    by=.(DRN, site, stream_type)]
+  
+  #data.table::setnames(in_sprich, 'Site', 'site') #not working for some reason 
+  #SET_STRING_ELT() can only be applied to a 'character vector', not a 'char'
+  in_sprich[, `:=`(site = Site,
+                   Site = NULL)]
+  
+  merged_dat <- merge(in_sprich, interm90_mean, by= 'site', all.x=T) %>%
+    merge(env_mean, by='site', all.x=T) %>%
+    .[site != 'BUK52',] %>% #Missing intermittence indicators according to Annika?
+    .[DRN == 'Czech Republic', DRN := 'Czech'] %>%
+    setnames('DRN', 'Country')
+  
+  return(merged_dat)
+}
--- _targets.R
+++ _targets.R
@@ -10,85 +10,88 @@ resdir <- 'results'
 
 tar_option_set(format = "qs")
 
-#------------------------- DEFINE PATHS ----------------------------------------
-#-------- WP2 data -------------------------------------------------------------
-bio_data_paths <- list(
-  dia_sedi = file.path(datdir, "diatoms_dna_sediment_removed_pools_and_zero_rows_and_columns.csv"),
-  dia_biof = file.path(datdir, "diatoms_dna_biofilm_removed_pools_and_zero_rows_and_columns.csv"),
-  fun_sedi = file.path(datdir, "fungi_dna_sediment_removed_pools_zero_rows_and_columns.csv"),
-  fun_biof = file.path(datdir, "fungi_dna_Biof_removed_pools_zero_rows_and_columns.csv"),
-  bac_sedi = file.path(datdir, "Sediment_bacteria_removed_pools_and_zero_species.csv"),
-  bac_biof = file.path(datdir, "Biofilm_bacteria_removed_pools_and_zero_species.csv"),
-  miv = file.path(datdir, "DRYvER_MIV_data_genus_reduced_taxa.csv"),
-  miv_nopools = file.path(datdir, "DRYvER_MIV_data_genus_reduced_taxa_removed_pools.csv"),
-  miv_nopools_flying = file.path(datdir, "DRYvER_MIV_data_genus_reduced_taxa_removed_pools_for_traits_flying.csv"),
-  miv_nopools_nonflying = file.path(datdir, "DRYvER_MIV_data_genus_reduced_taxa_removed_pools_for_traits_nonflying.csv")
-)
-
 metacols <- c('Campaign', 'Site', 'running_id', 'Country', 'Date',
-               'summa_sample', 'Sample.ID', 'Sample_ID', 'organism')
+              'summa_sample', 'Sample.ID', 'Sample_ID', 'organism')
 
 #--------------------------  Define targets plan -------------------------------
 list(
-  #------------------------------- Read files ----------------------------------
-  #Read in metadata
+  #------------------------------- Define paths ----------------------------------
+  tar_target(
+    env_data_path, 
+    file.path(datdir, "ENV_all_NAs_as_blank.csv"),
+    format='file'
+  ),
+  
+  tar_target(
+    metadata_edna_path,
+    file.path('data', 'metadata_DRYvER_eDNA_updated.xlsx'),
+    format='file'
+  ),
+  
+  tar_target(
+    bio_data_paths,
+    list(
+      dia_sedi = file.path(datdir, "diatoms_dna_sediment_removed_pools_and_zero_rows_and_columns.csv"),
+      dia_biof = file.path(datdir, "diatoms_dna_biofilm_removed_pools_and_zero_rows_and_columns.csv"),
+      fun_sedi = file.path(datdir, "fungi_dna_sediment_removed_pools_zero_rows_and_columns.csv"),
+      fun_biof = file.path(datdir, "fungi_dna_Biof_removed_pools_zero_rows_and_columns.csv"),
+      bac_sedi = file.path(datdir, "Sediment_bacteria_removed_pools_and_zero_species.csv"),
+      bac_biof = file.path(datdir, "Biofilm_bacteria_removed_pools_and_zero_species.csv"),
+      miv = file.path(datdir, "DRYvER_MIV_data_genus_reduced_taxa.csv"),
+      miv_nopools = file.path(datdir, "DRYvER_MIV_data_genus_reduced_taxa_removed_pools.csv"),
+      miv_nopools_flying = file.path(datdir, "DRYvER_MIV_data_genus_reduced_taxa_removed_pools_for_traits_flying.csv"),
+      miv_nopools_nonflying = file.path(datdir, "DRYvER_MIV_data_genus_reduced_taxa_removed_pools_for_traits_nonflying.csv")
+    )
+    #, format='file'
+  ),
+  
+  tar_target(
+    interm90_data_paths,
+    lapply(c('Croatia', 'Czech Republic', 'Finland', 'France', 'Hungary', 'Spain'),
+           function(country) {
+             file.path(datdir, 'Datasets', 'Intermittence_Data',
+                       paste0(country, '_Local_Interm_90_d.csv'))
+           })
+  ),
+  
+  #------------------------------- Read in data ----------------------------------
+  tar_target(
+    env_dt,
+    fread(env_data_path)
+  ),
+  
+  tar_target(
+    interm90_dt,
+    lapply(interm90_data_paths, fread) %>% 
+      rbindlist 
+  ),
+  
+  tar_target(
+    metadata_edna,
+    read.xlsx(metadata_edna_path, 
+              sheetName = 'metadataDNA') %>%
+      as.data.table
+  ),
+  
   tar_target(
     bio_dt,
-    read_biodt(path_list=bio_data_paths)
+    read_biodt(path_list = bio_data_paths,
+               in_metadata_edna = metadata_edna)
   ),
-
+  
   tar_target(
-   sprich,
-   lapply(bio_dt, function(dt) {
-     calc_sprich(in_biodt=dt, in_metacols=metacols)
-   }) %>% rbindlist
+    sprich,
+    lapply(bio_dt, function(dt) {
+      calc_sprich(in_biodt=dt, 
+                  in_metacols=metacols)
+    }) %>% rbindlist
+  )
+  ,
+  
+  tar_target(
+    alphadat_merged,
+    merge_alphadat(in_env_dt = env_dt,
+                  in_interm90_dt = interm90_dt,
+                  in_sprich = sprich)
   )
 )
-  
-
-
-# 
-#   
-#   
-# )
-# 
-# # UPDATE 1: to update bacteria, open old diversity data
-# divdata <- fread("C:/Users/vilmi/Desktop/DRYVERR/2024/Results/Mean_diversity.csv")
-# 
-# 
-# names(bac_biof_S_mean)
-# 
-# names(divdata)
-# divdata$V1 <- NULL
-# names(divdata)
-# 
-# divupdate <- rbind(divdata, bac_biof_S_mean)
-# 
-# # reorder columns in bacteria sediment diversity dataframes
-# names(bac_sed_S_mean)
-# bac_sed_S_mean2 <- bac_sed_S_mean[,c(3,1,2)]
-# names(bac_sed_S_mean2)
-# 
-# divupdate <- rbind(divupdate, bac_sed_S_mean2)
-# 
-# #save as csv
-# write.csv(divupdate, file = "C:/Users/vilmi/Desktop/DRYVERR/2024/Results/Mean_diversity_updated.csv")
-# 
-# 
-# 
-# # UPDATE 2, 15.4.2024: update flying and non-flying macroinvertebrates, with no pools data
-# 
-# divdata <- fread("C:/Users/vilmi/Desktop/DRYVERR/2024/Results/Mean_diversity_updated.csv")
-# 
-# names(flymac_S_mean)
-# names(nonflymac_S_mean)
-# names(divdata)
-# divdata$V1 <- NULL
-# names(divdata)
-# 
-# 
-# divupdate2 <- rbind(divdata, flymac_S_mean, nonflymac_S_mean)
-# 
-# 
-# #save as csv
-# write.csv(divupdate2, file = "C:/Users/vilmi/Desktop/DRYVERR/2024/Results/Mean_diversity_updated2.csv", row.names = F)
