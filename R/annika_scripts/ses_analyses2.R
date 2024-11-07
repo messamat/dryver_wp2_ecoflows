e6e4e4bf6907de592cdb1c4cac1bc5b06c7c05ad
messamat <messamat@uw.edu>
2024-11-07 14:30:11 +0100

    finish transcribing ses_analyses2

--- R/annika_scripts/ses_analyses2.R
+++ R/annika_scripts/ses_analyses2.R
@@ -1,303 +1,124 @@
 
 
 
-res <- tar_read(null_models) %>%
-  .[, Country := str_to_title(Country)] %>%
-  .[Country == 'Czech', Country := 'Czech Republic']
 
 
-env <- tar_read(env_dt) %>%
-  setDT %>%
-  data.table::setnames(c('site', 'DRN'),
-                       c('Site', 'Country')) %>%
-  .[Country == 'Czech', Country := 'Czech Republic']
+SES_TotDur90 <- all[, cor.test(z, TotDur90), by=.(Country, organism)] 
+SES_discharge <- all[, cor.test(z, discharge), by=.(Country, organism)] 
 
-int <- tar_read(interm90_dt) %>%
-  setDT %>%
-  data.table::setnames('Sites', 'Site')
-
-int90 <- int[, list(TotDur90 = mean(TotDur, na.rm=T),
-                    TotLeng90 = mean(TotLeng, na.rm=T)), by=.(Site, Country)]
-env_mean <- env[, list(discharge = mean(discharge_l_s, na.rm=T),
-                       moss = mean(moss_cover, na.rm=T),
-                       particle_size = mean(particle_size, na.rm=T)
-                       )
-                , by=.(Site, Country, stream_type)]
-
-all <- merge(res, int90, by=c("Site", "Country"), all.x=T, sort=F) %>%
-  merge(env_mean, by=c("Site", "Country"), all.x=T, sort=F) %>%
-  .[Site != 'BUK52', ] #Intermittence indicators are missing here
-
-SES_TotDur90_fun_sedi <- all[organism=='fun_sedi',
-                             cor.test(z, TotDur90),
-                             by=Country] 
-SES_discharge_fun_sedi <- all[organism=='fun_sedi',
-                             cor.test(z, discharge),
-                             by=Country] 
-
-
-plots <- list()
-for(i in unique(all$Country)){
-  d <- subset(all, Country == i)
-  plots[[paste0(i)]] <- ggplot(d[organism=="miv"], aes(x=z, y=Site, color=stream_type)) + 
-    scale_colour_manual(values = c("steelblue","orange")) +
-    geom_boxplot() + coord_flip() +
-    ggtitle(paste(i)) +
-    theme_classic() +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-}
-
-pdf(file.path(resdir, "Null_models", "Macroinvertebrates_SES_vs_streamtype.pdf"), 
-    height=10, width=10)
-do.call('grid.arrange', c(plots))
-dev.off()
-
-
-#### scatterplot, all countries in the same plot, separately for each organism group ####
-
-all[, significance := fifelse(pval <= 0.05, "sig", "nonsig")]
-
-ss <- all[organism == "dia_sedi"] 
-ss2 <- ss[significance == "sig"]
-# TotDur90
-# summary(lm(ss$SES~ ss$TotDur90))
-
-p.vals = sapply(unique(ss$Country), function(i) {
-  coef(summary(lm(z~TotDur90, data=ss[Country==i, ])))[2,4] 
-})
-p.vals
-p1 <- ggplot() + 
-  geom_point(aes(TotDur90, z, colour=Country), data = ss, size = 1, alpha=0.2) +
-  geom_point(aes(TotDur90, z, colour=Country), data = ss2, size = 1, alpha=0.5) +
-  geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals < 0.05],], aes(TotDur90, z, colour=Country), method = "lm", linewidth = 0.75, se = F) + 
-  geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals > 0.05],], aes(TotDur90, z, colour=Country), method = "lm", linewidth = 0.75, se = F, linetype="dashed") +
-  geom_smooth(aes(TotDur90, z), data=ss, colour="black", method = "lm", linewidth = 1.1, se = F) + # , linetype="dashed"
-  theme_classic() +
-  ggtitle("Sediment diatoms") + # MUOKKAA
-  xlab("Number of dry days") +
-  ylab("z") +
-  scale_color_manual(values = c("Croatia" = "#ef476f",
-                                "Czech Republic" = "#f78c6b", 
-                                "Finland" = "#ffd166", 
-                                "France" = "#06d6a0", 
-                                "Hungary" = "#118ab2",
-                                "Spain" = "#073b4c")) +
-  theme(legend.position = "none")
-p1
-pdf(file.path(resdir, "Null_models", "All_Sediment_diatoms_SES_vs_TotDur90_lm2.pdf"), height=3, width=4) # MUOKKAA
-p1
-dev.off()
 
 # TotLeng90
-summary(lm(ss$z~ ss$TotLeng90))
+for (in_organism in unique(in_env_null_models_dt$organism)) {
+  print(in_organism)
+  ss <- in_env_null_models_dt[organism == in_organism] 
+  ss2 <- ss[significance == "sig",]
+  p.vals = sapply(unique(ss$Country), function(i) {
+    coef(summary(lm(z~TotLeng90, data=ss[Country==i, ])))[2,4] 
+  })
+  p1 <- ggplot() + 
+    geom_point(aes(TotLeng90, z, colour=Country), data = ss, size = 1, alpha=0.2) +
+    geom_point(aes(TotLeng90, z, colour=Country), data = ss2, size = 1, alpha=0.5) +
+    geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals < 0.05],], aes(TotLeng90, z, colour=Country), method = "lm", linewidth = 0.75, se = F) + 
+    geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals > 0.05],], aes(TotLeng90, z, colour=Country), method = "lm", linewidth = 0.75, se = F, linetype="dashed") +
+    geom_smooth(aes(TotLeng90, z), data=ss, colour="black", method = "lm", linewidth = 1.1, se = F) + # , linetype="dashed"
+    theme_classic() +
+    ggtitle("Sediment diatoms") + # MUOKKAA
+    xlab("Duration of dry periods") +
+    ylab("z") +
+    scale_color_manual(values = c("Croatia" = "#ef476f",
+                                  "Czech Republic" = "#f78c6b", 
+                                  "Finland" = "#ffd166", 
+                                  "France" = "#06d6a0", 
+                                  "Hungary" = "#118ab2",
+                                  "Spain" = "#073b4c")) +
+    theme(legend.position = "none")
+  
+  pdf(file.path(resdir, "Null_models", 
+                paste0("All_", in_organism, "_SES_vs_TotLeng90_lm2.pdf")), 
+      height=3, width=4)
+  print(p1)
+  dev.off()
+}
 
-p.vals = sapply(unique(ss$Country), function(i) {
-  coef(summary(lm(z~TotLeng90, data=ss[Country==i, ])))[2,4] 
-})
-p.vals
-p1 <- ggplot() +
-  geom_point(aes(TotLeng90, z, colour=Country), data = ss, size = 1, alpha=0.2) +
-  geom_point(aes(TotLeng90, z, colour=Country), data = ss2, size = 1, alpha=0.5) +
-  geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals < 0.05],], aes(TotLeng90, z, colour=Country), method = "lm", linewidth = 0.75, se = F) + 
-  geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals > 0.05],], aes(TotLeng90, z, colour=Country), method = "lm", linewidth = 0.75, se = F, linetype="dashed") +
-  geom_smooth(aes(TotLeng90, z), data=ss, colour="black", method = "lm", linewidth = 1.1, se = F) + # , linetype="dashed"
-  theme_classic() +
-  ggtitle("Sediment diatoms") + # MUOKKAA
-  xlab("Duration of dry periods") +
-  ylab("z") +
-  scale_color_manual(values = c("Croatia" = "#ef476f",
-                                "Czech Republic" = "#f78c6b", 
-                                "Finland" = "#ffd166", 
-                                "France" = "#06d6a0", 
-                                "Hungary" = "#118ab2",
-                                "Spain" = "#073b4c")) +
-  theme(legend.position = "none")
-p1
-
-pdf(file.path(resdir, "Null_models", "All_Sediment_diatoms_SES_vs_TotLeng90_lm2.pdf"), 
-    height=3, width=4) 
-p1
-dev.off()
 
 #Discharge
-p.vals = sapply(unique(ss$Country), function(i) {
-  coef(summary(lm(z~discharge, data=ss[Country==i, ])))[2,4] 
-})
-p.vals
-p1 <- ggplot() +
-  geom_point(aes(discharge, z, colour=Country), data = ss, size = 1, alpha=0.2) +
-  geom_point(aes(discharge, z, colour=Country), data = ss2, size = 1, alpha=0.5) +
-  geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals < 0.05],], aes(discharge, z, colour=Country), method = "lm", linewidth = 0.75, se = F) + 
-  geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals > 0.05],], aes(discharge, z, colour=Country), method = "lm", linewidth = 0.75, se = F, linetype="dashed") +
-  geom_smooth(aes(discharge, z), data=ss, colour="black", method = "lm", linewidth = 1.1, se = F) + # , linetype="dashed"
-  theme_classic() +
-  ggtitle("Sediment diatoms") + # MUOKKAA
-  xlab("Discharge") +
-  ylab("z") +
-  scale_color_manual(values = c("Croatia" = "#ef476f",
-                                "Czech Republic" = "#f78c6b", 
-                                "Finland" = "#ffd166", 
-                                "France" = "#06d6a0", 
-                                "Hungary" = "#118ab2",
-                                "Spain" = "#073b4c")) +
-  theme(legend.position = "none")
-p1
-
-pdf(file.path(resdir, "Null_models", "All_Sediment_diatoms_SES_vs_Discharge_lm2.pdf"), 
-    height=3, width=4) 
-p1
-dev.off()
-
+for (in_organism in unique(in_env_null_models_dt$organism)) {
+  print(in_organism)
+  ss <- in_env_null_models_dt[organism == in_organism] 
+  ss2 <- ss[significance == "sig",]
+  p.vals = sapply(unique(ss$Country), function(i) {
+    coef(summary(lm(z~discharge, data=ss[Country==i, ])))[2,4] 
+  })
+  p1 <- ggplot() + 
+    geom_point(aes(discharge, z, colour=Country), data = ss, size = 1, alpha=0.2) +
+    geom_point(aes(discharge, z, colour=Country), data = ss2, size = 1, alpha=0.5) +
+    geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals < 0.05],], aes(discharge, z, colour=Country), method = "lm", linewidth = 0.75, se = F) + 
+    geom_smooth(data=ss[ss$Country %in% names(p.vals)[p.vals > 0.05],], aes(discharge, z, colour=Country), method = "lm", linewidth = 0.75, se = F, linetype="dashed") +
+    geom_smooth(aes(discharge, z), data=ss, colour="black", method = "lm", linewidth = 1.1, se = F) + # , linetype="dashed"
+    theme_classic() +
+    ggtitle("Sediment diatoms") + # MUOKKAA
+    xlab("Discharge") +
+    ylab("z") +
+    scale_color_manual(values = c("Croatia" = "#ef476f",
+                                  "Czech Republic" = "#f78c6b", 
+                                  "Finland" = "#ffd166", 
+                                  "France" = "#06d6a0", 
+                                  "Hungary" = "#118ab2",
+                                  "Spain" = "#073b4c")) +
+    theme(legend.position = "none")
+  
+  pdf(file.path(resdir, "Null_models", 
+                paste0("All_", in_organism, "_SES_vs_discharge_lm2.pdf")), 
+      height=3, width=4)
+  print(p1)
+  dev.off()
+}
 
 #### mixed models ####
-lmer_bac_sedi <- all[
-  organism=='bac_sedi_nopools',
+lmer_int <- all[,
   list(
-    TotDur90_full = lmer(z ~ TotDur90 + (1|Country), data=.SD),
-    TotDur90_null = lmer(z ~ (1|Country), data=.SD),
+    TotDur90_full = list(lmer(z ~ TotDur90 + (1|Country), data=.SD)),
+    TotDur90_null = list(lmer(z ~ (1|Country), data=.SD)),
     TotDur90_ML = list(anova(lmer(z ~ TotDur90 + (1|Country), data=.SD),
                         lmer(z ~ (1|Country), data=.SD))),
-    TotLeng90_full = lmer(z ~ TotLeng90 + (1|Country), data=.SD),
-    TotLeng90_null = lmer(z ~ (1|Country), data=.SD),
+    TotLeng90_full = list(lmer(z ~ TotLeng90 + (1|Country), data=.SD)),
+    TotLeng90_null = list(lmer(z ~ (1|Country), data=.SD)),
     TotLeng90_ML = list(anova(lmer(z ~ TotLeng90 + (1|Country), data=.SD),
                          lmer(z ~ (1|Country), data=.SD)))
   )
-  ,] 
+  , by=organism] 
 
 
 #### jitter plots to show deviation of SES from zero, all countries in the same plot, separately for each organism group ####
-ss <- all[organism == "bac_biof_nopools"] 
-jitter1 <- ggplot(ss) + 
-  scale_y_continuous() +
-  geom_jitter(aes(Country, z, colour=significance), shape=16, size = 2, alpha=0.7, position=position_jitter(0.2)) +
-  theme_classic() +
-  theme(axis.title.x = element_blank()) +
-  labs(y = "z") +
-  ggtitle("Biofilm bacteria") +
-  scale_color_manual(values = c("Significant" = "mediumblue",
-                                "Non-significant" = "lightslateblue")) +
-  labs(colour = "Departure from zero") +
-  theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1))
-# theme(legend.position = "bottom")
-# theme(legend.position = "none")
-plot(jitter1)
-# pdf(paste("M:/gDRYvER/WP 2/Metacommunity models/Tulokset/Null models/Jitterplot_with_legend.pdf"), height=3, width=4) # MUOKKAA
-# jitter1
-# dev.off()
+z_jitter_by_organism <- function(in_dt) {
+  jitter_p <- ggplot(in_dt) + 
+    scale_y_continuous() +
+    geom_jitter(aes(Country, z, colour=significance), 
+                shape=16, size = 2, alpha=0.7, position=position_jitter(0.2)) +
+    theme_classic() +
+    theme(axis.title.x = element_blank()) +
+    labs(y = "z") +
+    ggtitle(unique(in_dt$organism)) +
+    scale_color_manual(values = c("sig" = "mediumblue",
+                                  "nonsig" = "lightslateblue")) +
+    labs(colour = "Departure from zero") +
+    theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
+          legend.position = 'non')
+  return(jitter_p)
+}
 
-ss <- all[organism == "bac_sedi_nopools"] 
-jitter2 <- ggplot(ss) + 
-  scale_y_continuous() +
-  geom_jitter(aes(Country, z, colour=significance), shape=16, size = 2, alpha=0.7, position=position_jitter(0.2)) +
-  theme_classic() +
-  theme(axis.title.x = element_blank()) +
-  labs(y = "z") +
-  ggtitle("Sediment bacteria") +
-  scale_color_manual(values = c("Significant" = "mediumblue",
-                                "Non-significant" = "lightslateblue")) +
-  labs(colour = "Departure from zero") +
-  theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-  # theme(legend.position = "bottom")
-  theme(legend.position = "none")
-plot(jitter2)
-
-ss <- all[organism == "dia_biof"] 
-jitter3 <- ggplot(ss) + 
-  scale_y_continuous() +
-  geom_jitter(aes(Country, z, colour=significance), shape=16, size = 2, alpha=0.7, position=position_jitter(0.2)) +
-  theme_classic() +
-  theme(axis.title.x = element_blank()) +
-  labs(y = "z") +
-  ggtitle("Biofilm diatoms") +
-  scale_color_manual(values = c("Significant" = "mediumblue",
-                                "Non-significant" = "lightslateblue")) +
-  labs(colour = "Departure from zero") +
-  theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-  # theme(legend.position = "bottom")
-  theme(legend.position = "none")
-plot(jitter3)
-
-levels(factor(all$organism))
-
-ss <- all[organism == "dia_sedi"] 
-jitter4 <- ggplot(ss) + 
-  scale_y_continuous() +
-  geom_jitter(aes(Country, z, colour=significance), shape=16, size = 2, alpha=0.7, position=position_jitter(0.2)) +
-  theme_classic() +
-  theme(axis.title.x = element_blank()) +
-  labs(y = "z") +
-  ggtitle("Sediment diatoms") +
-  scale_color_manual(values = c("Significant" = "mediumblue",
-                                "Non-significant" = "lightslateblue")) +
-  labs(colour = "Departure from zero") +
-  theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-  # theme(legend.position = "bottom")
-  theme(legend.position = "none")
-plot(jitter4)
-
-levels(factor(all$all))
-
-ss <- all[organism == "fun_biof"] 
-jitter5 <- ggplot(ss) + 
-  scale_y_continuous() +
-  geom_jitter(aes(Country, z, colour=significance), shape=16, size = 2, alpha=0.7, position=position_jitter(0.2)) +
-  theme_classic() +
-  theme(axis.title.x = element_blank()) +
-  labs(y = "z") +
-  ggtitle("Biofilm fungi") +
-  scale_color_manual(values = c("Significant" = "mediumblue",
-                                "Non-significant" = "lightslateblue")) +
-  labs(colour = "Departure from zero") +
-  theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-  # theme(legend.position = "bottom")
-  theme(legend.position = "none")
-plot(jitter5)
-
-ss <- all[organism == "fun_sedi"] 
-jitter6 <- ggplot(ss) + 
-  scale_y_continuous() +
-  geom_jitter(aes(Country, z, colour=significance), shape=16, size = 2, alpha=0.7, position=position_jitter(0.2)) +
-  theme_classic() +
-  theme(axis.title.x = element_blank()) +
-  labs(y = "z") +
-  ggtitle("Sediment fungi") +
-  scale_color_manual(values = c("Significant" = "mediumblue",
-                                "Non-significant" = "lightslateblue")) +
-  labs(colour = "Departure from zero") +
-  theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-  # theme(legend.position = "bottom")
-  theme(legend.position = "none")
-plot(jitter6)
-
-levels(factor(all$all))
-
-ss <- all[organism == "miv"] 
-jitter7 <- ggplot(ss) + 
-  scale_y_continuous() +
-  geom_jitter(aes(Country, z, colour=significance), shape=16, size = 2, alpha=0.7, position=position_jitter(0.2)) +
-  theme_classic() +
-  theme(axis.title.x = element_blank()) +
-  labs(y = "z") +
-  ggtitle("Macroinvertebrates") +
-  scale_color_manual(values = c("Significant" = "mediumblue",
-                                "Non-significant" = "lightslateblue")) +
-  labs(colour = "Departure from zero") +
-  theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-  # theme(legend.position = "bottom")
-  theme(legend.position = "none")
-plot(jitter7)
+plots_jitter <- list()
+for (in_organism in unique(all$organism)) {
+  print(in_organism)
+  plots_jitter[[in_organism]] <- z_jitter_by_organism(in_dt = all[organism==in_organism,])
+}
 
 pdf(file.path(resdir, "Null_models", "Jitterplots_SES_significance.pdf"),
     height=10, width=6)
-grid.arrange(jitter1, 
-             jitter2, 
-             jitter3,
-             jitter4,
-             jitter5,
-             jitter6, 
-             jitter7, 
-             ncol=2)
+do.call("grid.arrange", c(plots_jitter, ncol=2))
 dev.off()
+
+
 
 ###############################################################################################
 # rm(list = ls())
