ab12d26fc9de5d3444e4c87637f769a5e41c6075
Mathis MEssager <mathis.messager@mail.mcgill.ca>
2025-01-21 17:30:49 +0100

    Create a first version of the function to correct the IDs associated with the shapefiles that apparently works for Croatia

--- in_construction_re_assign_ids.R
+++ in_construction_re_assign_ids.R
@@ -30,17 +30,6 @@ hydro_combi <- expand.grid(
 #Problem: 'cat' or 'reach_id' in the shapefile, associated with lines across 
 #confluences. Makes it impossible to correctly use network hydrology data.
 
-
-#Prepare a network without splits between confluences  by removing all pseudonodes
-
-
-#Then, iteratively:
-#Starting upstream in first order streams with IDs that are only on one line, and among them, the shortest lines
-#Assign the ID that is a first-order stream in reach.par of which this line represents the largest portion in the network
-#Deal with all first-order streams this way, making sure that any stream that was dealt with before cannot be reassigned.
-
-#If a reach ID disappeared because of this process. Grab Q of downstream reach id, and assign "dry" in any of the two are dry.
-
 #Parameters
 
 in_country <- 'Croatia'
@@ -87,8 +76,13 @@ rivnet_smooth_inters_nopseudo <- as_sfnetwork(rivnet_smooth_inters) %>%
 #Compute UID length
 rivnet_smooth_inters_nopseudo$length_uid <- as.numeric(st_length(rivnet_smooth_inters_nopseudo))
 
-#Compute other lengths
+#Create data.table to work with
 rivnet_inters_dt <- as.data.table(rivnet_smooth_inters_nopseudo) 
+
+#Back up cat before edit
+rivnet_inters_dt[, cat_copy := cat]
+
+#Compute other lengths
 rivnet_inters_dt[, length_cat := sum(length_uid), by=cat]
 rivnet_inters_dt[, length_smooth := sum(length_uid), by=UID_smooth]
 
@@ -97,6 +91,9 @@ rivnet_inters_dt[, length_smooth := sum(length_uid), by=UID_smooth]
 rivnet_inters_dt[, length_uid_catper := length_uid/length_cat]
 #Check whether this is the largest segment section for that cat 
 rivnet_inters_dt[, length_uid_catmax := (length_uid == max(length_uid)), by=cat]
+#Compute the percentage length of that full segment that is represented by
+#that segment section (identified by UID)
+rivnet_inters_dt[, length_uid_smoothper := length_uid/length_smooth]
 
 #Computer number of full segments that a cat overlaps
 rivnet_inters_dt[, n_seg_overlap := length(unique(UID_smooth)), by=cat]
@@ -105,7 +102,7 @@ rivnet_inters_dt[, n_seg_overlap := length(unique(UID_smooth)), by=cat]
 #keep that cat for this section of the segment
 #making sure that length_uid_catmax is TRUE is to deal with cats that are split
 #between non-contiguous segment sections (see cat==2938 for Croatia)
-rivnet_inters_dt[n_seg_overlap == 1 & strahler == 1 & length_uid_catmax, 
+rivnet_inters_dt[n_seg_overlap == 1 & strahler == 1, 
                  cat_smooth := cat]
 
 cats_assigned <- rivnet_inters_dt[!is.na(cat_smooth), unique(cat_smooth)]
@@ -116,67 +113,193 @@ rivnet_inters_dt[length_uid == length_smooth & !(cat %in% cats_assigned),
 
 cats_assigned <- rivnet_inters_dt[!is.na(cat_smooth), unique(cat_smooth)]
 
-#For first order segments, for cats for which at least 50% of the length is 
-#in that segment, assign the cat to that segment section
-rivnet_inters_dt[strahler == 1 & length_uid_catper > 0.5, ]
+#For cats that are already assigned, remove their "cat" from other segments
+rivnet_inters_dt[cat %in% cats_assigned & is.na(cat_smooth), cat := NA]
 
-############TO BE CONTINUED
+#Re-compute length_uid_smoothper, excluding segment sections whose initial
+#cat was assigned elsewhere
+rivnet_inters_dt[!is.na(cat), length_uid_smoothper := length_uid/sum(length_uid),
+                 by=UID_smooth]
 
-##########################################################
-########################################################
+#For those segments where only one cat remains, 
+#assign this cat to the entire segment
+cat_smooth_toassign <-  rivnet_inters_dt[
+  is.na(cat_smooth) & length_uid_smoothper == 1 & length_uid/length_smooth > (1/3),
+  .(UID_smooth, cat)] %>%
+  setnames('cat', 'cat_smooth')
+rivnet_inters_dt[is.na(cat_smooth), 
+                 cat_smooth := cat_smooth_toassign[.SD, on='UID_smooth', 
+                                                   cat_smooth]]
 
-#Re-assign cat from cat_smooth
+cats_assigned <- rivnet_inters_dt[!is.na(cat_smooth), unique(cat_smooth)]
+
+#For cats that are already assigned, remove their "cat" from other segments
+rivnet_inters_dt[cat %in% cats_assigned & is.na(cat_smooth), cat := NA]
+
+#For segment section with cat==NAs in first order streams, assign cat_smooth of
+#upstream segment
+cat_smooth_toassign <- rivnet_inters_dt[!is.na(cat_smooth) & (strahler == 1) & 
+                   (to %in% rivnet_inters_dt[is.na(cat) & strahler == 1, unique(from)]), 
+                 .(to, UID_smooth, cat_smooth)] %>%
+  setnames('to', 'from')
+rivnet_inters_dt[is.na(cat) & strahler == 1,
+                 cat_smooth := cat_smooth_toassign[
+                   .SD, on=c('from', 'UID_smooth'), cat_smooth]]
+
+#Re-compute length_uid_smoothper, excluding segment sections whose initial
+#cat was assigned elsewhere
+rivnet_inters_dt[!is.na(cat), length_uid_smoothper := length_uid/sum(length_uid),
+                 by=UID_smooth]
+
+#For those segments where only one cat remains, 
+#assign this cat to the entire segment
+cat_smooth_toassign <-  rivnet_inters_dt[
+  is.na(cat_smooth) & length_uid_smoothper == 1 & length_uid/length_smooth > (1/3),
+  .(UID_smooth, cat)] %>%
+  setnames('cat', 'cat_smooth')
+rivnet_inters_dt[is.na(cat_smooth), 
+                 cat_smooth := cat_smooth_toassign[.SD, on='UID_smooth', 
+                                                   cat_smooth]]
+
+cats_assigned <- rivnet_inters_dt[!is.na(cat_smooth), unique(cat_smooth)]
+
+#For cats that are already assigned, remove their "cat" from other segments
+rivnet_inters_dt[cat %in% cats_assigned & is.na(cat_smooth), cat := NA]
+
+#Computer number of full segments that a cat overlaps
+rivnet_inters_dt[, n_seg_overlap := length(unique(UID_smooth)), by=cat]
+
+#For second order segments where the cat is only represented by that segment,
+#keep that cat for this section of the segment
+rivnet_inters_dt[n_seg_overlap == 1 & strahler <= 2 & is.na(cat_smooth), 
+                 cat_smooth := cat]
+
+cats_assigned <- rivnet_inters_dt[!is.na(cat_smooth), unique(cat_smooth)]
+
+#For cats that are already assigned, remove their "cat" from other segments
+rivnet_inters_dt[cat %in% cats_assigned & is.na(cat_smooth), cat := NA]
+
+#For segment section with cat==NAs in first order streams, assign cat_smooth of
+#upstream segment
+cat_smooth_toassign <- rivnet_inters_dt[!is.na(cat_smooth) & (strahler == 1) & 
+                                          (to %in% rivnet_inters_dt[is.na(cat) & strahler == 1, unique(from)]), 
+                                        .(to, UID_smooth, cat_smooth)] %>%
+  setnames('to', 'from')
+rivnet_inters_dt[is.na(cat) & strahler == 1,
+                 cat_smooth := cat_smooth_toassign[
+                   .SD, on=c('from', 'UID_smooth'), cat_smooth]]
+
+#Re-compute length_uid_smoothper, excluding segment sections whose initial
+#cat was assigned elsewhere
+rivnet_inters_dt[!is.na(cat), length_uid_smoothper := length_uid/sum(length_uid),
+                 by=UID_smooth]
+
+#For those segments where only one cat remains, 
+#assign this cat to the entire segment
+cat_smooth_toassign <-  rivnet_inters_dt[
+  is.na(cat_smooth) & length_uid_smoothper == 1 & length_uid/length_smooth > (1/3),
+  .(UID_smooth, cat)] %>%
+  setnames('cat', 'cat_smooth')
+rivnet_inters_dt[is.na(cat_smooth), 
+                 cat_smooth := cat_smooth_toassign[.SD, on='UID_smooth', 
+                                                   cat_smooth]]
+
+cats_assigned <- rivnet_inters_dt[!is.na(cat_smooth), unique(cat_smooth)]
+
+#For cats that are already assigned, remove their "cat" from other segments
+rivnet_inters_dt[cat %in% cats_assigned & is.na(cat_smooth), cat := NA]
+
+#For sections that represent over 75% of the total length of that cat, assign cat_smooth
+rivnet_inters_dt[length_uid_catper>0.7 & is.na(cat_smooth), cat_smooth := cat]
+
+
+#For cats that are already assigned, remove their "cat" from other segments
+rivnet_inters_dt[cat %in% cats_assigned & is.na(cat_smooth), cat := NA]
+
+#Re-compute length_uid_smoothper, excluding segment sections whose initial
+#cat was assigned elsewhere
+rivnet_inters_dt[!is.na(cat), length_uid_smoothper := length_uid/sum(length_uid),
+                 by=UID_smooth]
+
+#For those segments where only one cat remains, 
+#assign this cat to the entire segment
+cat_smooth_toassign <-  rivnet_inters_dt[
+  is.na(cat_smooth) & length_uid_smoothper == 1 & length_uid/length_smooth > (1/3),
+  .(UID_smooth, cat)] %>%
+  setnames('cat', 'cat_smooth')
+rivnet_inters_dt[is.na(cat_smooth), 
+                 cat_smooth := cat_smooth_toassign[.SD, on='UID_smooth', 
+                                                   cat_smooth]]
+
+cats_assigned <- rivnet_inters_dt[!is.na(cat_smooth), unique(cat_smooth)]
+
+#For cats that are already assigned, remove their "cat" from other segments
+rivnet_inters_dt[cat %in% cats_assigned & is.na(cat_smooth), cat := NA]
+
+#For segment sections in strahler==1 & n_seg_overlap > 2, cat := NA
+rivnet_inters_dt[strahler == 1 & n_seg_overlap > 2 & is.na(cat_smooth), 
+                 cat := NA] 
+
+#if only representative of cat, assign cat_smooth
 rivnet_inters_dt[!is.na(cat_smooth), cat := cat_smooth]
-
-#Re-compute total cat length
 rivnet_inters_dt[, length_cat := sum(length_uid), by=cat]
+rivnet_inters_dt[, length_uid_catper := length_uid/length_cat, by=cat]
+rivnet_inters_dt[length_uid_catper > 0.7 & is.na(cat_smooth), cat_smooth := cat]
 
-#Then, iteratively:
-#Starting upstream in first order streams with IDs that are only on one line, and among them, the shortest lines
-#Assign the ID that is a first-order stream in reach.par of which this line represents the largest portion in the network
-#Deal with all first-order streams this way, making sure that any stream that was dealt with before cannot be reassigned.
+#For cats that are already assigned, remove their "cat" from other segments
+cats_assigned <- rivnet_inters_dt[!is.na(cat_smooth), unique(cat_smooth)]
+rivnet_inters_dt[cat %in% cats_assigned & is.na(cat_smooth), cat := NA]
 
+#Re-compute length_uid_smoothper, excluding segment sections whose initial
+#cat was assigned elsewhere
+rivnet_inters_dt[!is.na(cat_smooth), 
+                 length_uid_smoothper := length_uid/sum(length_uid),
+                 by=UID_smooth]
+cat_smooth_toassign <-  rivnet_inters_dt[
+  is.na(cat_smooth) & length_uid_smoothper == 1,
+  .(UID_smooth, cat)] %>%
+  setnames('cat', 'cat_smooth')
+rivnet_inters_dt[is.na(cat_smooth), 
+                 cat_smooth := cat_smooth_toassign[.SD, on='UID_smooth', 
+                                                   cat_smooth]]
 
-rivnet_inters_dt[cat==2960,]
-rivnet_inters_dt[UID_smooth==569,]
+#For the remaining segments with all NAs, 
+#assign the cat of the section representing the highest percent of the segment
+rivnet_inters_dt[is.na(cat_smooth), NAlength := sum(length_uid),
+                 by=UID_smooth]
+rivnet_inters_dt[(NAlength==length_smooth), 
+                 cat_smooth := fifelse(length_uid == max(length_uid), cat_copy, NA), 
+                 by=UID_smooth]
 
+#For all remaining sections, remove their "cat" 
+rivnet_inters_dt[is.na(cat_smooth), cat := NA]
 
+for (i in 1:2) {
+  #For segment section with cat==NAs and cat_smooth==NA, 
+  #assign cat_smooth of upstream segment of same strahler order
+  cat_smooth_toassign <- rivnet_inters_dt[
+    !is.na(cat_smooth) &
+      (from %in% rivnet_inters_dt[is.na(cat) & is.na(cat_smooth), unique(to)]), 
+    .(from, UID_smooth, cat_smooth, strahler)] %>%
+    setnames('from', 'to')
+  rivnet_inters_dt[is.na(cat) & is.na(cat_smooth),
+                   cat_smooth := cat_smooth_toassign[
+                     .SD, on=c('to', 'UID_smooth'), cat_smooth]]
+  
+  #For is.na(cat) & is.na(cat_smooth), 
+  #assign downstream cat_smooth of same UID_smooth
+  cat_smooth_toassign <- rivnet_inters_dt[
+    !is.na(cat_smooth) &
+      (to %in% rivnet_inters_dt[is.na(cat) & is.na(cat_smooth), unique(from)]), 
+    .(to, UID_smooth, cat_smooth, strahler)] %>%
+    setnames('to', 'from')
+  rivnet_inters_dt[is.na(cat) & is.na(cat_smooth),
+                   cat_smooth := cat_smooth_toassign[
+                     .SD, on=c('from', 'UID_smooth'), cat_smooth]]
+}
 
-
-
-ggplotly(
-ggplot(rivnet_smooth_inters) +
-  geom_sf(aes(color=cat))
-)
-
-
-#Only consider hydromod_reaches that are represented in the shapefile
-reaches_sub <- in_reaches_dt[ID %in% rivnet$cat,]
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Preformat basic network
-sfnet_ini <- rivnet %>%
-  as_sfnetwork %>%
-  activate("edges") %>%
-  filter(!edge_is_multiple()) %>% #Keep shortest of edges that connect the same pair of nodes
-  filter(!edge_is_loop()) #Remove obvious loops: edges that start and end at the same node
-
-#------------------ Split lines at intersections -----------------------------
-#Get confluence nodes (nodes of third degree: with at least 3 intersecting edges)
-#2nd degree nodes are pseudonodes and 1st degree nodes are dangling
-splitting_nodes <- activate(sfnet_ini, nodes) %>%
-  mutate(degree = igraph::degree(.)) %>%
-  filter(degree >= 3) %>%
-  st_as_sf("nodes")
-
+#Check
+write_sf(merge(rivnet_smooth_inters_nopseudo, 
+               rivnet_inters_dt[, .(cat_smooth, UID)],
+               by='UID')[, c('UID', 'cat', 'strahler', 'UID_smooth', 'cat_smooth')],
+         file.path(resdir, 'scratch.gdb'), layer='test_cat16')
