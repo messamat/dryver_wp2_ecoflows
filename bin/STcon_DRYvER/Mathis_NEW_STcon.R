56b3b66659843bc8f07babaf6b9dc229a01b0a5b
Mathis MEssager <mathis.messager@mail.mcgill.ca>
2025-02-21 13:51:34 +0100

    Get D. Cunillera's code

---
+++ bin/STcon_DRYvER/Mathis_NEW_STcon.R
@@ -0,0 +1,199 @@
+
+# load libraries
+library(tidyverse)
+library(ncdf4)
+library(lubridate)
+library(reshape2)
+library(sf)
+library(devtools)
+library(igraph)
+library(riverconn)
+
+working_directory <- paste("/STcon_DRYvER",sep="")
+setwd(working_directory)
+
+# load functions
+source("functions_flow_intermittence_indicators.R")
+# The code from GitHUB includes plots and network centrality calculations
+#source("https://raw.github.com/Cunillera-Montcusi/Quantifyinig-SpaTem-connectivity/main/SpaTemp_function.R")
+# You are interested in the "depurated" version of the code
+source("SpaTemp_function_Mathis.R")
+
+DryRiverNet <- c("Albarine", "Bukkosdi", "Lepsamaanjoki", "Genal","Butiznica","Velicka")
+
+DRN_position=1 # For looping between catchments 
+
+#_____________________________________________________________________________________________________________________
+# Import data following WP1 codes and functions to upload the flow_intermittence 
+catchment <- DryRiverNet[DRN_position] # select the catchment name 
+nc_file <- get_results_file_present(catchment)
+nc <- nc_open(nc_file) # open netcdf file
+reachID <- ncvar_get(nc, "reachID") # get list of reaches IDs
+dates <- ncvar_get(nc, "date") # get dates of simulation period
+dates <- as.Date(dates, origin="1950-01-01") # convert dates into R date format
+flow_intermittence <- get_nc_var_present(nc, "isflowing", reachID, dates) # 0=dry, 1=flowing
+
+#___________________________________________________________________
+# WARNING 1
+# Some flow intermittence values for some dates are NA, it is not necessarily for all dates but it is something 
+# to take into account. I assign them 1 in case that they exist. 
+flow_intermittence$value[is.na(flow_intermittence$value)] <- 1 
+#__________________________________________________________________
+
+# Shape names & uploadign
+Shape_country <- c("france","hungary","finland","spain","croatia","czech")
+shape_DRN <- sf::st_read(paste(MSI_New,"Desktop/dryver_shapefiles_cleaned/",Shape_country[DRN_position],"_river_network_cleaned.shp",sep=""))
+
+#___________________________________________________________________
+# WARNING 2
+# Croatian stream has two weird reaches.
+# Reach 9001 is actually not going anywhere, its from and to are not connected to anything. I eliminate it. 
+# Reach 1176 does not exist in the flow intermittence and there is no "1100" cat for the entire catchment. I suspect, 
+#that it is a "typo" of some kind. The from and to from this reach connect to reaches with cat 1776, which made me think
+#on this as a typo. I edited that reach to 1776 to get the corresponding assignation. 
+if(DRN_position==5){
+  shape_DRN <- shape_DRN %>% filter(cat!=9001)
+  shape_DRN[which(shape_DRN$cat==1176),4] <- 1776 }
+#___________________________________________________________________
+
+#___________________________________________________________________
+# WARNING 3
+# Some reaches have the same "cat" so I change their name adding a LETTER for the duplicated ones. 
+#this is just ot later assign the outlet as igraph does not want duplicated reaches.
+cat_to_correct <- shape_DRN %>% arrange(from) %>% group_by(cat) %>% mutate(count=n()) %>%  filter(count>1)%>% pull(cat)
+if (length(cat_to_correct)>0) {
+  shape_DRN[which(shape_DRN$cat%in%cat_to_correct),4]$cat <- paste(cat_to_correct,rep(LETTERS[1:length(which(cat_to_correct-cat_to_correct[1]==0))]),sep="_")
+}
+#___________________________________________________________________
+
+# We built the adjacency list to built the graph
+DRN_adj_table <- shape_DRN  %>% arrange(from) %>% select(from,to,to_cat_shp) %>% sf::st_drop_geometry()
+# We identify where the outlet is (the NA and to_cat_shp)
+Outlet_from_to <- as.numeric(DRN_adj_table[which(is.na(DRN_adj_table$to_cat_shp)==T),c("to")] %>%  sf::st_drop_geometry())
+# We create the graph from the DRN_adj_list (from - to)
+igr1 <- graph_from_adj_list(DRN_adj_table[1:2])
+# We need to incorporate a new vertex corresponding the outlet (that we have detecte in the previous step). 
+# We name the cat 11111 and it will be our last vertex and where the outlet will be directed.  
+cat_shape <- shape_DRN %>% arrange(from) %>% select(cat,from) %>%sf::st_drop_geometry() %>% 
+             mutate(cat=as.character(cat)) %>% 
+             bind_rows(data.frame("cat"="11111",from=Outlet_from_to)) %>% arrange(from)
+# We assign vertex attributes according to the cat_shape, ordered following the "from" value from the cat_shape, 
+# which is not exactly "from anymore" it is an ID of the vertex
+V(igr1)$cat <- as.character(cat_shape$cat)
+# Using riverconn package (Damiano is the best) we set the directionality towards 11111 vertex. Our outlet.
+# This is a key step to ensure that the network ends where it needs to end! Otherwise MISTAKE! 
+igr1 <- set_graph_directionality(igr1, field_name = "cat",outlet_name =  "11111")
+
+# To ease the plotting and posterior management we create an "edge_DaFr" where we put the coordinates of the  
+# starting and ending points of the segments and we edit the x and y coordinates. 
+edges_DaFr<- left_join(
+as.data.frame(st_line_sample(shape_DRN, sample = 0) %>% st_coordinates())%>% 
+  left_join(shape_DRN %>% select(UID,from,to) %>% st_drop_geometry(),by=c("L1"="UID")) %>% 
+  rename(X1_coord=X,Y1_coord=Y),
+as.data.frame(st_line_sample(shape_DRN, sample = 1) %>% st_coordinates()) %>% 
+  left_join(shape_DRN %>% select(UID,from,to) %>% st_drop_geometry(),by=c("L1"="UID")) %>% 
+  rename(X2_coord=X,Y2_coord=Y),
+by=c("from","to"))
+  
+# Second step is to built a data.frame with the starting coordinates of all the segments and add 
+# the initial row of the ending coordinates (which corresponds to the last point of the graph so the endpoint). 
+nodes_df <- as.data.frame(st_line_sample(shape_DRN, sample = 0) %>% st_coordinates()) %>% 
+            bind_rows(as.data.frame(st_line_sample(shape_DRN, sample = 1) %>% st_coordinates())[1,]) %>% 
+            select(L1,"x"=X,"y"=Y) %>% mutate(L1_Copy=L1,.after=L1)
+
+ggplot()+
+  geom_segment(data=edges_DaFr, 
+               aes(x=X1_coord,y=Y1_coord, xend=X2_coord, yend=Y2_coord), 
+               arrow =arrow(length=unit(0.4,"cm"), ends="last"), linewidth=0.2, colour="grey50", alpha=1)+
+  geom_point(data=nodes_df, aes(x=x, y=y), shape=21)+
+  theme_classic()
+
+# We create the "End_point" site that will correspond to the 111111 in the flow_intermittence dataset
+# this point will be added with the same frequency of any other reach
+End_point <- flow_intermittence %>% filter(reachID==flow_intermittence$reachID[1]) %>%# We select whatever reach
+                                    rename("cat"="reachID") %>% 
+                                    mutate(cat="11111",value=1) %>% #Modify its values as we wish 
+                                    select(dates, cat, value) # select only this three values to merge them later
+
+# Now last step to "merge" everything together into the Intermittence dataset
+# We merge the shape_DRN dataset with the flow intermittence throw splitting the repeated "cat"
+# and making sure that we are assigning the corresponding value of intermittence to the reach and 
+# at the same time we maintain the "cat ids" that were used to built the graph. 
+# Two options exists because not all DRN have repeated values
+# We merge the Endpoint site and we "pivot_wide" the table to obtain the TRUE intermittence table, 
+# where each row corresponds to a day (dates as factors) and columns to all nodes of the network.
+if (length(cat_to_correct)>0) {
+  Inermitence_dataset <- 
+    shape_DRN %>% select(UID,from,to,cat) %>% st_drop_geometry() %>% 
+    mutate(TRUE_cat=unlist(lapply(strsplit(cat,split = "_"), `[`, 1))) %>% 
+    left_join(flow_intermittence,by=c("TRUE_cat"="reachID"),relationship = "many-to-many")%>%
+    select(dates, cat, value) %>% 
+    bind_rows(End_point) %>% 
+    pivot_wider(id_cols = dates,names_from = cat,values_from = value) %>% 
+    mutate(dates=as.factor(dates))
+}else{
+  Inermitence_dataset <- 
+    shape_DRN %>% select(UID,from,to,cat) %>% st_drop_geometry() %>% 
+    mutate(cat=as.factor(cat)) %>% 
+    left_join(flow_intermittence,by=c("cat"="reachID"),relationship = "many-to-many")%>%
+    select(dates, cat, value) %>% 
+    bind_rows(End_point) %>% 
+    pivot_wider(id_cols = dates,names_from = cat,values_from = value) %>% 
+    mutate(dates=as.factor(dates))
+  }
+
+# CONGRATS! All is ready to STcon things! 
+
+# You cut the lenght of the intermittence according to whatever you want. In this case we select the first 
+# 30 days of the whole dataset. 
+FL_intermitence_cut <- as.data.frame(Inermitence_dataset[1:30,]) # THIS IS THE MOST IMPORTANT POINT! WHERE YOU DEFINE THE TIME WINDOW!!!! 
+
+# We built the matrix of the network structure for the STcon, which is the "base" on which connectivity will be assessed. 
+Network_structure <- as.data.frame(as.matrix(as_adjacency_matrix(igr1)))
+
+# Last comprobation with some indicators. FL_intermittence has to have 1 more (dates) column
+cat("There are",nrow(nodes_df),"sites.",ncol(FL_intermitence_cut),"columns in the intermitence dataset &",dim(Network_structure),"network rows and columns.")
+
+# We put everything in lists as the funcion parallelizes things according to the number of things inside 
+# the number of objects inside the lists! So, everything must be entered as a list! 
+# For example: 
+# In case of calculating STcon for a DRN in different dates you could create a list with 
+# the DRN information into lists where each element of the list corresopnds to a date or whatever. 
+# In this examples we just have 1 element per list.
+Inermitence_dataset_Campaings_To_Run <- list(FL_intermitence_cut)
+Sites_coordinates_Campaings_To_Run <- list(nodes_df)
+Network_stru_Campaings_To_Run <- list(Network_structure)
+
+# We add an extra "reference river" into the lists with a REFerence intermitence where all sites are permanent (so = 1)
+REF_FL_intermitence<-FL_intermitence_cut %>% replace(.==0,1)
+# We add the seventh "reference river into the list
+Inermitence_dataset_Campaings_To_Run[[length(Inermitence_dataset_Campaings_To_Run)+1]] <- REF_FL_intermitence
+Sites_coordinates_Campaings_To_Run[[length(Sites_coordinates_Campaings_To_Run)+1]] <- nodes_df
+Network_stru_Campaings_To_Run[[length(Network_stru_Campaings_To_Run)+1]] <- Network_structure
+
+
+# Here we STCON things! 
+DirNonW <- spat_temp_index(Inermitence_dataset = Inermitence_dataset_Campaings_To_Run,
+                           Sites_coordinates=Sites_coordinates_Campaings_To_Run,
+                           Network_stru = Network_stru_Campaings_To_Run, 
+                           direction="directed", sense= "out",
+                           weighting=F,dist_matrices = NULL, # Weighting pairs
+                           weighting_links =F,link_weights = NULL, # Weighting links
+                           legacy_effect = 1, legacy_lenght = 1, # Legacy effects
+                           value_S_LINK=1,value_T_LINK=1, # Values to links
+                           value_NO_S_link=0,value_NO_T_link=0, # Values to links
+                           ) # Last parameters information
+
+
+STcon <- DirNonW$STcon[[1]]/DirNonW$STcon[[2]]
+STcon <- STcon[-is.na(STcon)]
+
+ggplot()+
+  geom_segment(data=edges_DaFr %>%mutate(STcon=STcon), 
+               aes(x=X1_coord,y=Y1_coord, xend=X2_coord, yend=Y2_coord,colour=STcon), 
+               arrow =arrow(length=unit(0.01,"cm"), ends="last"), linewidth=1.3, alpha=1)+
+  geom_point(data=nodes_df,aes(x=x, y=y),fill="grey",shape=21,alpha=0.5)+
+  scale_color_viridis()+
+  theme_classic()
+
+
---
+++ bin/STcon_DRYvER/functions_flow_intermittence_indicators.R
@@ -0,0 +1,186 @@
+#########################################################################################
+## Import data
+#########################################################################################
+# watershed
+import_watershed <- function(catchment)
+{
+  watershed_shp_PATH <- paste0("data/",catchment,"/watershed_small_catchment.shp")
+  watershed <- st_read(watershed_shp_PATH) 
+  
+  watershed
+}
+
+# reaches
+import_reaches <- function(catchment, watershed)
+{
+  reach_shp_PATH <- paste0("data/",catchment,"/river_network.shp")
+  reaches <- st_read(reach_shp_PATH) 
+  
+  # intersection of reaches and watershed
+  reaches$cnt = stringr::str_count(reaches$geometry, ",")
+  reaches = filter(reaches, cnt>1)
+  reaches <- st_intersection(reaches, watershed)
+  
+  reaches
+}
+
+get_results_file_present <- function(catchment)
+{
+  directory <- paste0("data/",catchment,"/Results_present_period/")
+  files <- list.files(directory)
+  files <- files[-which(files == "readme.txt")]
+  if (length(files) == 1)
+  {
+    paste0(directory,files)
+  }
+  else if (length(files) == 0)
+  {
+    print(paste0("Error : no file(s) in ","data/",catchment,"/Results_present_period/"))
+  }else{
+    day <- strsplit(files,"_")
+    day <- unlist(lapply(day, function(x) substr(x[2],1,10)))
+    day <- as.Date(day, format="%Y-%m-%d")
+    k <- which(day == max(day))
+    paste0(directory,files[k])
+  }
+}
+
+get_nc_var_present <- function(nc, varname, reachID, dates)
+{
+  data <- ncvar_get(nc, varname)
+  data <- as.data.frame(data)
+  colnames(data) <- reachID
+  data$dates <- dates
+  
+  hy <- hsaHydroYearSeasons(data$dates, month = 10, day_of_the_year = TRUE, # compute hydrological years, and other time periods (seasons, month, civil year, day of the year)
+                              seasons = list("SummerFall" = 5:10, "WinterSpring" = c(11:12, 1:4)),
+                              seasons_by_day = FALSE, minimal = FALSE)
+  
+  data$doy <- hy$j # month
+  data$month <- hy$m # month
+  data$season <- hy$s # season
+  data$year <- hy$y # year
+  data$yearHy <- hy$hy # hydrological year
+  
+  data <- melt(data, id.vars=c("dates","doy","month","season","year","yearHy"))
+  colnames(data)[colnames(data) == "variable"] <- "reachID"
+
+  data
+}
+
+# -------------------------------------------------------------------
+# Compute a data.frame containing the hydrological year, and 
+# optionnally the days of the hydrological year and the season which
+# each day belongs to. (developped by Ivan Horner)
+# -------------------------------------------------------------------
+# dates:        a date/time vector (will be coerced to date using 
+#               as.Dates())
+# month:        month defining the start of a hydrological year
+# day_of_the_year: should the days of the (hydrological) year be
+#               computed?
+# seasons:      list defining the seasons in month number (1:12)
+#               (or days if 'seasons_by_day' is TRUE).
+# seasons_by_day: are seasons defined by days (of the year)?
+# minimal:      should only the minimal desired results be returned
+#               or intermediate results as well?
+hsaHydroYearSeasons <- function(dates, month = 9, day_of_the_year = TRUE, 
+                                seasons = list("SummerFall" = 5:10, "WinterSpring" = c(11:12, 1:4)),
+                                seasons_by_day = FALSE, minimal = FALSE) {
+  dates <- as.Date(dates)
+  
+  # get hydrological year
+  m <- as.numeric(format(dates, format = "%m"))
+  hy <- y <- as.numeric(format(dates, format = "%Y"))
+  m_prevy <- !m%in%c(month:12)
+  hy[m_prevy] <- hy[m_prevy] - 1
+  
+  # get days of the year
+  if (day_of_the_year) {
+    j <- as.numeric(format(dates, format = "%j"))
+    start_hy <- as.Date(paste0(y, "-", month, "-1"))
+    start_y <- as.Date(paste0(y, "-1-1"))
+    j_hy <- as.numeric(format(dates - start_hy + start_y, format = "%j"))
+  } else {
+    j_hy <- NA
+  }
+  
+  # get seasons
+  if (!is.null(seasons) && is.list(seasons)) {
+    # check season formatting
+    if (!all(vapply(seasons, is.atomic, logical(1L)))) {
+      stop("Wrong formatting for 'seasons': it should be a named list containing only atomic vectors (e.g. not lists).")
+    }
+    s_names <- names(seasons)
+    s_names <- factor(s_names, levels = s_names)
+    # build a table with character string and corresponding factors for matching with the whole time series
+    s_table <- data.frame(i = unname(unlist(seasons)), season = rep(s_names, unlist(lapply(seasons, length))))
+    if (seasons_by_day && day_of_the_year){
+      s <- s_table[match(j, s_table[, 1]), 2]
+    } else {
+      if (seasons_by_day) stop("If 'seasons_by_day' is TRUE, you must also have 'day_of_the_year' set to TRUE.")
+      
+      s <- s_table[match(m, s_table[, 1]), 2]
+    }
+    if (minimal) return(data.frame(hy = hy, j_hy = j_hy, s = s))
+    data.frame(dates = dates, m = m, y = y, hy = hy, j = j, j_hy = j_hy, s = s)
+  } else {
+    if (minimal) return(data.frame(hy = hy, j_hy = j_hy))
+    data.frame(dates = dates, m = m, y = y, hy = hy, j = j, j_hy = j_hy)
+  }
+}
+
+#########################################################################################
+## Flow intermittence indicators
+#########################################################################################
+# Indicator 1 : Number of days with dry/flowing conditions per timescale
+# flow_intermittence : dataframe with flow intermittence data
+# condition : "dry" or "flowing"
+# time_scale : "yearHy" (hydrological year) or "season" or "month"
+# reaches : stream network (sf object)
+indicator_1 <- function(flow_intermittence, condition, time_scale, reaches)
+{
+  condition_abr <- "F"
+  if(condition == "dry")
+  {
+    flow_intermittence$value <- ifelse(flow_intermittence$value == 0,1,0)
+    condition_abr <- "D"
+  }
+  
+  k <- which(colnames(flow_intermittence) == time_scale)
+  flow_intermittence$time_scale <- flow_intermittence[,k]
+  df <- flow_intermittence %>%
+    group_by(time_scale, yearHy, reachID) %>%
+    summarise(nb_days=sum(value)) %>%
+    ungroup()
+  
+  if(time_scale == "yearHy")
+  {
+    df <- df %>%
+      group_by(reachID) %>%
+      summarise(mean_nb_days=mean(nb_days, na.rm=FALSE))
+    
+    name <- paste0("Con",condition_abr)
+    reaches$indicator <- sapply(reaches$cat, function(x){if(x %in% df$reachID){df$mean_nb_days[df$reachID == x]}else{NA}})
+    colnames(reaches)[colnames(reaches) == "indicator"] <- name
+    
+  }else{
+    df <- df %>%
+      group_by(time_scale, reachID) %>%
+      summarise(mean_nb_days=mean(nb_days, na.rm=FALSE))
+    df <- df %>% dcast(reachID ~ time_scale, value.var = "mean_nb_days")
+    #df$reachID <- as.numeric(as.character(df$reachID))
+    
+    names <- paste0("Con",condition_abr,"_",colnames(df)[-1])
+    for (i in 1:length(names))
+    {
+      reaches <-cbind(reaches,sapply(reaches$cat, function(x){if(x %in% df$reachID){df[df$reachID == x, i+1]}else{NA}}))
+      colnames(reaches)[grepl("sapply",colnames(reaches))] <- names[i]
+    }
+  }
+  
+  reaches
+}
+
+
+
+
